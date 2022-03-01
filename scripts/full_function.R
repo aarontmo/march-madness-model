d2e96a9e88b497baee2079c5df069aa4a79633cf
Morris <aarontmo@byui.edu>
2022-03-01 13:28:58 -0700

    start predict function

---
+++ scripts/full_function.R
@@ -0,0 +1,12 @@
+match_predicter <- function(team1, team2, seed1, seed2){
+  
+  # load libraries
+  library(tidyverse)
+  
+  # load data
+  reg_season <- read_csv("https://github.com/aarontmo/march-madness-model/raw/master/raw_data/cbb.csv")
+  # write web scrape in separate scipt to get these, links on github
+  schedule_strength <- read_csv("")
+  odds <- read_csv("")
+  
+}
